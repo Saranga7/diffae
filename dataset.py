4ca7d02eefb6aad949fb1409030f8edd1d3cdd15
Saranga MAHANTA <mahanta@gpuclust01.bioclust.biologie.ens.fr>
2024-08-26 19:06:28 +0200

    modify

--- dataset.py
+++ dataset.py
@@ -176,8 +176,8 @@ class FFHQlmdb(Dataset):
             transform.append(transforms.ToTensor())
         if do_normalize:
             transform.append(
-                # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) Moi korisu
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) #Moi korisu
                 )
                 
             
